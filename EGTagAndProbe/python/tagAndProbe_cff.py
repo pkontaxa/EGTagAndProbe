4ad0554beb93cadf30a8ac94c24840366f924013
pkontaxa <pantykont@gmail.com>
2017-12-20 16:19:01 +0200

    Updated cfg file_data

--- EGTagAndProbe/python/tagAndProbe_cff.py
+++ EGTagAndProbe/python/tagAndProbe_cff.py
@@ -9,8 +9,8 @@ import HLTrigger.HLTfilters.hltHighLevel_cfi as hlt
 
 HLTLISTTAG = cms.VPSet(
     cms.PSet (
-        HLT = cms.string("HLT_Ele27_WPTight_Gsf_v"),
-        path1 = cms.vstring ("hltEle27WPTightGsfTrackIsoFilter"), #FIXME: to check
+        HLT = cms.string("HLT_Ele32_WPTight_Gsf_v"),
+        path1 = cms.vstring ("hltEle32WPTightGsfTrackIsoFilter"), #FIXME: to check
         path2 = cms.vstring (""),
         leg1 = cms.int32(11),
         leg2 = cms.int32(999)
@@ -19,12 +19,12 @@ HLTLISTTAG = cms.VPSet(
 
 HLTLISTPROBE = cms.VPSet(
     cms.PSet (
-        HLT = cms.string("HLT_Ele27_WPTight_Gsf_v"),
-        path1 = cms.vstring ("hltEle27WPTightGsfTrackIsoFilter"), #FIXME: to check
+        HLT = cms.string("HLT_Ele32_WPTight_Gsf_v"),
+        path1 = cms.vstring ("hltEle32WPTightGsfTrackIsoFilter"), #FIXME: to check
         path2 = cms.vstring (""),
         leg1 = cms.int32(11),
         leg2 = cms.int32(999)
-        ),
+        ),  
 )
 
 
@@ -33,7 +33,7 @@ HLTLISTPROBE = cms.VPSet(
 import HLTrigger.HLTfilters.hltHighLevel_cfi as hlt
 hltFilter = hlt.hltHighLevel.clone(
     TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
-    HLTPaths = ['HLT_Ele27_WPTight_Gsf_v*'],
+    HLTPaths = ['HLT_Ele32_WPTight_Gsf_v*'],
     andOr = cms.bool(True), # how to deal with multiple triggers: True (OR) accept if ANY is true, False (AND) accept if ALL are true
     throw = cms.bool(True) #if True: throws exception if a trigger path is invalid
 )
@@ -61,7 +61,7 @@ Ntuplizer = cms.EDAnalyzer("Ntuplizer",
 
     triggerResultsLabel = cms.InputTag("TriggerResults", "", "HLT"),   
     L1EG = cms.InputTag("caloStage2Digis", "EGamma", "RECO"),
-    L1EmuEG = cms.InputTag("simCaloStage2Digis", "MP"),
+    L1EmuEG = cms.InputTag("simCaloStage2Digis"),
     Vertices = cms.InputTag("offlinePrimaryVertices"),
     triggerListTag = HLTLISTTAG,
     triggerListProbe = HLTLISTPROBE,
