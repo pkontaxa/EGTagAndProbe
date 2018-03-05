a39bb91d294324cc264168c4bd92d5d78732a35f
Marina Kolosova <marina.kolosova@cern.ch>
2018-03-05 17:12:09 -0600

    Commented l1tEmuEG.seedTowerEt() and l1tEmuEG.seedTowerEm() due to compilation errors. Added CRAB config for Unpacked data.

--- EGTagAndProbe/plugins/Ntuplizer.cc
+++ EGTagAndProbe/plugins/Ntuplizer.cc
@@ -819,11 +819,11 @@ void Ntuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& eSetup)
 						this -> _l1tEmuRawEt     = l1tEmuEG.rawEt();
 						this -> _l1tEmuIsoEt     = l1tEmuEG.isoEt();
                                                 
-                                                this -> _seedTowerEt     = l1tEmuEG.seedTowerEt(); 
+                                                //this -> _seedTowerEt     = l1tEmuEG.seedTowerEt(); 
  
                                                 this -> _TowerHoE        = l1tEmuEG.towerHoE();  
    
-                                                this -> _iem             = l1tEmuEG.seedTowerEm();
+                                                //this -> _iem             = l1tEmuEG.seedTowerEm();
 
                                                 //this -> _ihad            = l1tEmuEG.seedTowerHad();
 
