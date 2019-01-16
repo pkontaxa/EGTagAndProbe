552459347b0e8c18634e58922ff4f1eb88843674
Marina <marina.kolosova@cern.ch>
2019-01-16 18:34:21 +0100

    NTuplizer: Simplified electronID sequence based on https://hypernews.cern.ch/HyperNews/CMS/get/egamma-elecid/133/1.html and organized file

--- EGTagAndProbe/test/test.py
+++ EGTagAndProbe/test/test.py
@@ -7,161 +7,128 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.Eras import eras
 
 #=====================================
-# Setup the process
-#=====================================
-# Marina
-process = cms.Process("TagAndProbe", eras.Run2_2018)
-
-#=====================================
-# Global Tag
-#=====================================
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
-
-#=====================================
 # Options
 #=====================================
-isMC = False
-isMINIAOD = True
+isMC        = False
+isMINIAOD   = True
+maxEvents   = 5000
+reportEvery = 100
+outputFile = 'NTuple.root'
 
-# Handling of cms line options for tier3 submission
-# The following are dummy defaults, so that one can normally use the config changing file list by hand etc.
-options = VarParsing.VarParsing ('analysis')
-options.register ('skipEvents',
-                  -1, # default value
-                  VarParsing.VarParsing.multiplicity.singleton, # Options = [singleton, list]
-                  VarParsing.VarParsing.varType.int,            # Options = [string, int, float]
-                  "Number of events to skip")
+if isMC:
+    globaltag = '102X_mcRun2_asymptotic_v3'
+    datasetFiles = [
+        '/store/mc/RunIIFall18MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v12_ext1-v1/60000/70E5316E-7F73-C94E-A6FC-9014BC36BDF0.root',
+        '/store/mc/RunIIFall18MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v12_ext1-v1/60000/70D11899-0FC2-5F4E-8E31-3586B5619B31.root',
+        '/store/mc/RunIIFall18MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v12_ext1-v1/60000/6ED73AA8-3C53-5A42-8E3B-EBF059924184.root',
+        '/store/mc/RunIIFall18MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v12_ext1-v1/60000/6DD6961E-C425-2B40-85F3-6082BB81D010.root',
+        ]
+else:
+    globaltag = '102X_dataRun2_Prompt_v4'
+    datasetFiles = [
+        '/store/data/Run2018D/EGamma/MINIAOD/PromptReco-v2/000/321/879/00000/7E4C974F-7EAE-E811-8701-FA163E735012.root',
+        '/store/data/Run2018D/EGamma/MINIAOD/PromptReco-v2/000/321/879/00000/78AF9DC9-78AE-E811-8171-FA163E529D85.root',
+        '/store/data/Run2018D/EGamma/MINIAOD/PromptReco-v2/000/321/879/00000/5E57E0BA-7FAE-E811-ABC2-FA163EA393DA.root',
+        '/store/data/Run2018D/EGamma/MINIAOD/PromptReco-v2/000/321/834/00000/E21A923C-46AE-E811-ABF0-02163E0153A6.root',
+        '/store/data/Run2018D/EGamma/MINIAOD/PromptReco-v2/000/321/834/00000/3448FF7B-3DAE-E811-BE8F-FA163E3F84E6.root',
+        ]
+    
 
-options.register ('JSONfile',
-                  "", # default value
-                  VarParsing.VarParsing.multiplicity.singleton, # Options = [singleton, list]
-                  VarParsing.VarParsing.varType.string,         # Options = [string, int, float]
-                  "JSON file (empty for no JSON)")
+# For debugging purposes
+debug = True
+RunNum_1    = 1
+LumiBlock_1 = 2
+EvtNum_1    = 240
+RunNum_2    = 1
+LumiBlock_2 = 2
+EvtNum_2    = 260
+    
+#========================================================
+# Setup the process
+#========================================================
+process = cms.Process("TagAndProbe", eras.Run2_2018)
 
-options.outputFile = 'NTuple.root'
-options.inputFiles = []
-options.maxEvents  = -999
-options.parseArguments()
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(debug),
+    # printDependencies = cms.untracked.bool(debug),
+    )
 
+process.maxEvents = cms.untracked.PSet(
+    input         = cms.untracked.int32(maxEvents)
+    )
 
-#======================================
-# Electron cut based ID section
-#======================================
-# Set up everything that is needed to compute electron IDs and
-# add the ValueMaps with ID decisions into the event data stream
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = reportEvery
 
-# Load tools and function definitions
+#========================================================
+# Define the input files
+#========================================================
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(datasetFiles)
+                            #eventsToProcess = cms.untracked.VEventRange('%s:%s:%s-%s:%s:%s' % (RunNum_1, LumiBlock_1, EvtNum_1, RunNum_2, LumiBlock_2, EvtNum_2) ),
+                            )
+
+#========================================================
+# Output File
+#========================================================
+process.TFileService = cms.Service('TFileService', fileName = cms.string(outputFile))
+
+#========================================================
+# Global Tag
+#========================================================
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+from Configuration.AlCa.autoCond import autoCond
+
+process.GlobalTag.globaltag = globaltag
+
+#===================================================
+# Electron ID
+#===================================================
 from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
-process.load("RecoEgamma.ElectronIdentification.ElectronMVAValueMapProducer_cfi")
 
-#**********************
-dataFormat = DataFormat.AOD
+dataFormat = DataFormat.MiniAOD
+
 if isMINIAOD:
     dataFormat = DataFormat.MiniAOD
+else:
+    dataFormat = DataFormat.AOD
+
 switchOnVIDElectronIdProducer(process, dataFormat)
-#**********************
-
-process.load("RecoEgamma.ElectronIdentification.egmGsfElectronIDs_cfi")
-# overwrite a default parameter: for miniAOD, the collection name is a slimmed one
-if isMINIAOD:
-    process.egmGsfElectronIDs.physicsObjectSrc = cms.InputTag('slimmedElectrons')
-
-from PhysicsTools.SelectorUtils.centralIDRegistry import central_id_registry
-process.egmGsfElectronIDSequence = cms.Sequence(process.egmGsfElectronIDs)
 
 # Define which IDs we want to produce
-# Each of these two example IDs contains all four standard 
 my_id_modules =[
-#    'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_25ns_V1_cff',        # both 25 and 50 ns cutbased ids produced
-#    'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_50ns_V1_cff',
-    'RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV60_cff',                     # recommended for both 50 and 25 ns
-#    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring15_25ns_nonTrig_V1_cff',     # will not be produced for 50 ns, triggering still to come
-#    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring15_25ns_Trig_V1_cff',        # 25 ns trig
-#    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring15_50ns_Trig_V1_cff',        # 50 ns trig
-    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_GeneralPurpose_V1_cff',   # Spring16
-    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_HZZ_V1_cff',              # Spring16 HZZ
-    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_iso_V1_cff', # Fall17
+    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_GeneralPurpose_V1_cff',
+    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_HZZ_V1_cff',
+    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_noIso_V1_cff',
+    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_iso_V1_cff',
+    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_noIso_V2_cff',
+    'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_iso_V2_cff',
     ] 
 
-
-#Add them to the VID producer
+# Add them to the VID producer
 for idmod in my_id_modules:
     setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
 
-egmMod = 'egmGsfElectronIDs'
-mvaMod = 'electronMVAValueMapProducer'
-regMod = 'electronRegressionValueMapProducer'
-egmSeq = 'egmGsfElectronIDSequence'
-setattr(process,egmMod,process.egmGsfElectronIDs.clone())
-setattr(process,mvaMod,process.electronMVAValueMapProducer.clone())
-setattr(process,regMod,process.electronRegressionValueMapProducer.clone())
-setattr(process,egmSeq,cms.Sequence(getattr(process,mvaMod)*getattr(process,egmMod)*getattr(process,regMod)))
-process.electrons = cms.Sequence(getattr(process,mvaMod)*getattr(process,egmMod)*getattr(process,regMod))
-
-# Marina
-if not isMC: # will use 80X
-    from Configuration.AlCa.autoCond import autoCond
-    process.GlobalTag.globaltag = '101X_dataRun2_Prompt_v9'
+#================================================================================================
+# Load processes
+#================================================================================================
+if not isMC:
     process.load('EGTagAndProbe.EGTagAndProbe.tagAndProbe_cff')
-    process.source = cms.Source("PoolSource",
-        fileNames = cms.untracked.vstring(
-            '/store/data/Run2018A/EGamma/MINIAOD/PromptReco-v1/000/315/259/00000/6864E4D1-654B-E811-BD3E-FA163ECB5162.root',
-        ),
-    )
 else:
-    process.GlobalTag.globaltag = '101X_mcRun2_asymptotic_v5'   # Marina
     process.load('EGTagAndProbe.EGTagAndProbe.MCanalysis_cff')
-    process.source = cms.Source("PoolSource",
-        fileNames = cms.untracked.vstring(            
-            '/store/mc/RunIISummer16DR80/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RAWAODSIM/FlatPU28to62HcalNZSRAWAODSIM_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v2/110000/005BFB99-EBA9-E611-8850-0CC47A4D762A.root'
-        )
-    )
     process.Ntuplizer.useHLTMatch = cms.bool(False) #In case no HLT object in MC sample considered or you're fed up with trying to find the right HLT collections
-
+    
+    
 if isMINIAOD:
     process.Ntuplizer.electrons    = cms.InputTag("slimmedElectrons")
     process.Ntuplizer.genParticles = cms.InputTag("prunedGenParticles")
     process.Ntuplizer.Vertices     = cms.InputTag("offlineSlimmedPrimaryVertices")
-
-if options.JSONfile:
-    print "Using JSON: " , options.JSONfile
-    process.source.lumisToProcess = LumiList.LumiList(filename = options.JSONfile).getVLuminosityBlockRange()
-
-if options.inputFiles:
-    process.source.fileNames = cms.untracked.vstring(options.inputFiles)
-
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
-)
-
-if options.maxEvents >= -1:
-    process.maxEvents.input = cms.untracked.int32(options.maxEvents)
-if options.skipEvents >= 0:
-    process.source.skipEvents = cms.untracked.uint32(options.skipEvents)
-
-process.options = cms.untracked.PSet(
-    wantSummary = cms.untracked.bool(True)
-)
-
+    
+#================================================================================================ 
+# Module execution
+#================================================================================================
 process.p = cms.Path(
-    process.electrons +
+    process.egmGsfElectronIDSequence *
     process.NtupleSeq
 )
-
-#process.schedule = cms.Schedule(process.p)
-
-
-# Automatic addition of the customisation function from L1Trigger.L1TNtuples.customiseL1Ntuple
-#from L1Trigger.L1TNtuples.customiseL1Ntuple import L1NtupleRAW
-
-#call to customisation function L1NtupleRAWEMU imported from L1Trigger.L1TNtuples.customiseL1Ntuple
-#process = L1NtupleRAW(process)
-
-
-# Silence output
-process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 1
-
-# Adding ntuplizer
-process.TFileService=cms.Service('TFileService',fileName=cms.string(options.outputFile))
